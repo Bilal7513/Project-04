b7cd9b63d8b780aea8ce6f1d3da3c46a0532c88a
Bilal Irfan <bi0755562@gmail.com>
2024-06-21 22:11:43 +0500

    Project 4 Complete
    Exchange Currency Rate Calculator

--- script.js
+++ script.js
@@ -14,20 +14,44 @@ function calculate(){
 
     // 
     fetch(` https://v6.exchangerate-api.com/v6/1ed57b0316eb63782be995d8/pair/${currencyOneCode}/${currencyTwoCode}`).then(res => res.json())
-    .then(data => console.log(data));
+    .then(data => {
+        // Get the conversion rate from currency one to currency two
+        const conversionRate = data.conversion_rate;
+
+        // Formating currency Two Amount
+        const amount2 = new Intl.NumberFormat('en-US').format((amountCurrencyOne.value * conversionRate).toFixed    (2));
+
+        // Update DOM
+        amountCurrencyTwo.value = amount2;
+        console.log(amount2);
+    }); 
 };
 
 // Event Listners
+
 // Recalculate exchange rate when currency 1 change
 currencyOne.addEventListener('change', calculate);
+
 // Recalculate exchange rate when currency 1 amount change 
 amountCurrencyOne.addEventListener('input', calculate);
 
 // Recalculate exchange rate when currency 2 change
 currencyTwo.addEventListener('change', calculate);
+
 // Recalculate exchange rate when currency 2 amount change 
 amountCurrencyTwo.addEventListener('input', calculate);
 
+// 
+swap.addEventListener('click', () => {
+    // Save value of currency one code to temp variable
+    const temp = currencyOne.value;
+    // Copy currency two code to currency one
+    currencyOne.value = currencyTwo.value;
+    // Copy currency one code from temp variable to currency two
+    currencyTwo.value = temp;
+    // Recalculate exchange rate after swap
+    calculate();
+});
 
 // Execute calculate function on page load
 calculate();
